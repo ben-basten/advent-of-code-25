74c34655cf62457919ea55d504904bac271f5ed4
ben-basten <45583362+ben-basten@users.noreply.github.com>
2025-12-03 22:10:12 -0500

    day 3 part 2

--- 3/3.ts
+++ 3/3.ts
@@ -27,7 +27,33 @@ const getJoltage = (battery: string) => {
 };
 
 function part2(input: Input): number {
-  return -1;
+  return input.reduce((total, current) => {
+    return total + getJoltagePart2(current);
+  }, 0);
 }
+
+const getJoltagePart2 = (battery: string) => {
+  const number = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+  for (let i = 0; i < battery.length; i++) {
+    const current = parseInt(battery[i]);
+    for (let base = 0; base < number.length; base++) {
+      if (
+        current > number[base] &&
+        i < battery.length - (number.length - 1 - base)
+      ) {
+        number[base] = current;
+        for (
+          let cleanIndex = base + 1;
+          cleanIndex < number.length;
+          cleanIndex++
+        ) {
+          number[cleanIndex] = 0;
+        }
+        break;
+      }
+    }
+  }
+  return parseInt(number.join(''));
+};
 
 export { marshalInput, part1, part2 };
